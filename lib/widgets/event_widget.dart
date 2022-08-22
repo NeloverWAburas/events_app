b8dc212ea6843ab06c1f32a96b9078d08759e154
FlutLab.io <>
2022-08-22 08:10:09 +0000

    Autosave

--- lib/widgets/event_widget.dart
+++ lib/widgets/event_widget.dart
@@ -5,33 +5,39 @@ class EventWidget extends StatelessWidget {
   const EventWidget({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      Image.asset("assets/images/Rectangle 2968.png"),
-      Column(
-         children: [
-        Text(
-          "12:00am-2hours",
-          style: TextStyle(
-            color: Colors.grey,
-          ),
-        ),
+    return SizedBox(
+      child: Row(children: [
+        Image.asset("assets/images/Rectangle 2968.png"),
         SizedBox(
-          height: 5,
-        ),
-        Text(
-          "Event Name",
-          style: TextStyle(fontSize: 20),
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        Text(
-          "12/12/2020",
-          style: TextStyle(
-            color: Colors.grey,
+          height: 50,
+          child: Column(
+            children: [
+              Text(
+                "12:00am-2hours",
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                "Event Name",
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                "12/12/2020",
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
           ),
-        ),
-      ],)
-    ]);
+        )
+      ]),
+    );
   }
 }
