31e118584e0880b09d10d937d35b218c733ed55d
FlutLab.io <>
2022-08-22 11:44:50 +0000

    Autosave

--- lib/screens/events_screen.dart
+++ lib/screens/events_screen.dart
@@ -47,7 +47,7 @@ class _EventsScreenState extends State<EventsScreen>
           overflow: Overflow.visible,
           alignment: Alignment.topCenter,
           children: [
-            Image.asset("assets/images/AppBarImage.png"),
+            Image.asset("assets/images/AppBarImage.png", fit: BoxFit.cover),
             Text(
               "Events",
               style: TextStyle(
