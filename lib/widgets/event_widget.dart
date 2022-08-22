f3edf08a54ef570621d688af1ff0929618253627
FlutLab.io <>
2022-08-22 08:10:29 +0000

    Autosave

--- lib/widgets/event_widget.dart
+++ lib/widgets/event_widget.dart
@@ -9,7 +9,7 @@ class EventWidget extends StatelessWidget {
       child: Row(children: [
         Image.asset("assets/images/Rectangle 2968.png"),
         SizedBox(
-          height: 50,
+          height: 55,
           child: Column(
             children: [
               Text(
